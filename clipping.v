ab8cb93983b102a9ce1c005d90de3a1ff271c97a
Sam Baker <Sam.Baker101@gmail.com>
2021-09-15 12:42:07 -0400

    Modified Clipping
    Modified Clipping Distortion

--- clipping.v
+++ clipping.v
@@ -1,12 +1,20 @@
 module clipping
-	#(parameter RESOLUTION = 24)
+	#(parameter RESOLUTION = 24,
+	SHIFT = 4)
 	(
 	input clk, enable,
 	input [RESOLUTION - 1:0]data_in,
-	output[RESOLUTION - 1:0]data_out
+	output reg[RESOLUTION - 1:0]data_out
 
 );
 
-assign data_out = enable ? data_in<<4 : data_in;
-
+	always @(posedge clk)
+		begin
+			if (!enable)
+				data_out <= data_in;
+			else begin
+				data_out[RESOLUTION - SHIFT - 1] <= |data_in[RESOLUTION-1:RESOLUTION - SHIFT - 1];
+				data_out <= data_in << SHIFT;
+			end
+		end
 endmodule
--- effects.v
+++ effects.v
@@ -11,7 +11,7 @@ module effects
 
 //Clipping - Bit shifts data left by 1 causing increased volume and distortion
 wire [RESOLUTION-1:0] L1;
-clipping #(RESOLUTION) clipL (.clk(data_CLK), .enable(SW[0]), .data_in(data_in), .data_out(L1));
+clipping #(.RESOLUTION(RESOLUTION), .SHIFT(6)) clipL (.clk(data_CLK), .enable(SW[0]), .data_in(data_in), .data_out(L1));
 
 //Echo
 echo #(.RESOLUTION(RESOLUTION), .DEPTH(4096)) eL (.clk(data_CLK), .enable(SW[1]), .data_in(L1), .data_out(data_out));
